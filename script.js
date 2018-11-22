073b90d0712dfdac7a2d5db278a2d4c6a3535c8b
Aleksandra <aleksandrayakovleva132@gmail.com>
2018-11-22 18:30:04 +0300

    Это наш первый коммит

---
+++ script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+a = 5;
+console.log(a);
+
+var number = 5;
+var string = "Hello!";
+var sym = Symbol();
+var boolean = true;
+null; 
+undefined;
+var obj = {};
+
+console.log(4/0);
+console.log('string'*9);
+
+var something;
+
+console.log(something);
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+
+
+console.log("2" === 2);
